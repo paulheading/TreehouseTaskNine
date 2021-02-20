d7464a167adc57d36083af2137df3e0369228331
Paul Heading <paul.heading@barbican.org.uk>
2021-02-20 20:44:01 +0000

    package & readme updated

--- src/components/Navigate/Navigate.js
+++ src/components/Navigate/Navigate.js
@@ -1,25 +1,32 @@
-import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
-import './Navigate.scss';
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import "./Navigate.scss";
 
 export default class Navigate extends Component {
-
-  constructor(props) {
-    super(props);
-  }
-
-  handleClick = delta => {
+  handleClick = (delta) => {
     this.props.changeLoading(true);
     this.props.changeSearch(delta);
-  }
+  };
 
   render() {
     return (
       <nav className="main-nav">
         <ul>
-          <li><NavLink to="/cats" onClick={ () => this.handleClick("cats") }>Cats</NavLink></li>
-          <li><NavLink to="/dogs" onClick={ () => this.handleClick("dogs") }>Dogs</NavLink></li>
-          <li><NavLink to="/pigs" onClick={ () => this.handleClick("pigs") }>Pigs</NavLink></li>
+          <li>
+            <NavLink to="/cats" onClick={() => this.handleClick("cats")}>
+              Cats
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dogs" onClick={() => this.handleClick("dogs")}>
+              Dogs
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/pigs" onClick={() => this.handleClick("pigs")}>
+              Pigs
+            </NavLink>
+          </li>
         </ul>
       </nav>
     );
